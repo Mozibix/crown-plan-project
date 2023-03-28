5ab623d2c983fdad063482619ab8d0b15431448d
Mozibix <opuzmoses@gmail.com>
2023-03-28 22:33:02 +0100

    chores: updating footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,58 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer_inner container_main flex_between">
+        <div className="footer_left_section flex_center">
+          <div className="footer_logo special_font flex_center">
+            Cro &nbsp;&nbsp;
+            <span>
+              <Image
+                src="/svgs/logo-icon.svg"
+                height={36}
+                className="logo_img"
+                width={36}
+                alt="logo"
+              />
+            </span>{" "}
+            n <span className="red_text">Plan</span>
+          </div>
+
+          <div className="privacy_terms">
+            <p>© 2020 Privacy — Terms</p>
+          </div>
+        </div>
+
+        <div className="footer_middle_section flex_center">
+          <Image
+            src="/svgs/facebook.svg"
+            width={30}
+            height={30}
+            alt="socials"
+          />
+          <Image src="/svgs/twitter.svg" width={30} height={30} alt="socials" />
+          <Image
+            src="/svgs/instagram.svg"
+            width={30}
+            height={30}
+            alt="socials"
+          />
+          <Image src="/svgs/wimeo.svg" width={30} height={30} alt="socials" />
+        </div>
+
+        <div className="footer_right_section">
+          <ul className="flex_center">
+            <li>ABOUT</li>
+            <li>FAQS</li>
+            <li>TERMS & CONDITIONS</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
