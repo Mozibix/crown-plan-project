b8e46fad4f51cce8b7a02b620f64293dd42242db
Mozibix <opuzmoses@gmail.com>
2023-03-28 22:33:18 +0100

    chores: updating header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,22 @@ import React from "react";
 const Header = () => {
   return (
     <nav>
-      <div className="nav_inner flex">
+      <div className="nav_inner  container_main">
         <div className="logo special_font">
-          Cro{" "}
+          Cro &nbsp;&nbsp;
           <span>
             <Image
               src="/svgs/logo-icon.svg"
-              height={25}
-              width={25}
+              height={55}
+              className="logo_img"
+              width={44}
               alt="logo"
             />
           </span>{" "}
-          nPlan
+          n <span className="red_text">Plan</span>
         </div>
 
-        <div className="links_sec">
+        <div className="desktop_links">
           <ul className="links">
             <li>Home</li>
             <li>Portfolio</li>
@@ -28,6 +29,28 @@ const Header = () => {
             <li>Contact</li>
           </ul>
         </div>
+
+        <div className="harmburger">
+          <Image
+            src="/svgs/burger.svg"
+            width={20}
+            height={20}
+            alt="harmurger"
+          />
+        </div>
+      </div>
+
+      {/* MOBILE-NAV */}
+
+      <div className="mobile_links">
+        <ul className="links">
+          <li>Home</li>
+          <li>Portfolio</li>
+          <li>Shop</li>
+          <li>About Us</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
       </div>
     </nav>
   );
