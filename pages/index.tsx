1a0bd2ae7a29c14fcb37c069a4d3a74024856056
Mozibix <opuzmoses@gmail.com>
2023-03-29 11:39:05 +0100

    chores: updating about_section

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,6 +30,44 @@ export default function Home() {
               <h3 className="header_text">ABOUT US</h3>
               <div className="line"></div>
             </div>
+
+            <div className="about_info">
+              <div className="about_left">
+                <Image
+                  src="/images/about-image.png"
+                  width={500}
+                  height={500}
+                  className="about_image"
+                  alt="about"
+                />
+              </div>
+
+              <div className="about_right">
+                <div className="about_right_inner">
+                  <div className="about_right_header">
+                    <h3 className="special_font">
+                      Let&apos;s Plan Your Perfect Wedding
+                    </h3>
+                  </div>
+                  <div className="about_right_quotes">
+                    <p>Wr&apos;re here to serve you, step-by-step</p>
+                    <p>
+                      Many people create notes on a computer and print them or
+                      send them through the internet. However, I believe the
+                      most effective notes are handwritten. A handwritten note
+                      is personal and rare. Due to this, it will be given more
+                      attention.
+                    </p>
+
+                    <p>
+                      A few weeks ago I was in Michigan for my dad&apos;s
+                      funeral. While there I attended my mom&apos;s church on
+                      Sunday and met her pastor for the second time.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
