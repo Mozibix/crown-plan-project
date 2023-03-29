d1acb565d4de8b27d7e605f334588305ba425e28
Mozibix <opuzmoses@gmail.com>
2023-03-29 01:07:11 +0100

    chores: updating hero_section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
 import MainLayout from "@/src/layouts/MainLayout";
+import Image from "next/image";
 export default function Home() {
   return (
     <MainLayout>
-      <div className="text-blue-900">helo worold</div>
+      <div className="body">
+        <div className="hero_section">
+          <div className="hero_section_inner ">
+            <div className="text_section">
+              <div className="hero_left special_font">
+                <h3>Wedding Planner</h3>
+              </div>
+
+              <div className="hero_right">
+                <p>
+                  The #1 ranked. Helped over 50,000 couples around the country
+                  aand we look forward to supporting you along your wedding
+                  planning journey.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* ABOUT SECTION */}
+
+        <div className="about_sec">
+          <div className="about_sec_inner container_main">
+            <div className="header_sec">
+              <h3 className="header_text">ABOUT US</h3>
+              <div className="line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </MainLayout>
   );
 }
