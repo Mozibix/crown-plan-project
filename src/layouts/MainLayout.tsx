ea70c0aae4fab1bf415ba5407c568b28d33217dc
Mozibix <opuzmoses@gmail.com>
2023-03-30 13:37:28 +0100

    chores: updating main-layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import React, { Fragment } from "react";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import Footer from "../(components)/Footer";
+import Header from "../(components)/Header";
 
 const MainLayout: React.FC<{ children: React.ReactElement }> = ({
   children,
