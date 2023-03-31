a7085b80fada678c9ab1506ddc543649daa8d19f
Mozibix <opuzmoses@gmail.com>
2023-03-31 01:18:54 +0100

    doc: updating tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      sm: { min: "578px" },
+      sm: { min: "640px" },
+      md: { min: "768px" },
+      lg: { min: "1024px" },
+      xl: { min: "1280px" },
+      "2xl": { min: "1536px" },
     },
   },
   plugins: [],
