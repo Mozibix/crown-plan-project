2aa29fd72951b7e6004046d77895ae54a2b9c979
Mozibix <opuzmoses@gmail.com>
2023-03-31 13:31:00 +0100

    chores: updating logo links

--- src/(components)/Footer.tsx
+++ src/(components)/Footer.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="footer">
       <div className="footer_inner container_main">
         <div className="footer_left_section flex_center">
-          <div className="footer_logo special_font">
-            Cro
-            <span>
-              <Image
-                src="/svgs/logo-icon.svg"
-                height={100}
-                className="logo_img"
-                width={100}
-                alt="logo"
-              />
-            </span>{" "}
-            n <span className="red_text">Plan</span>
-          </div>
+          <Link href="/">
+            <div className="footer_logo special_font">
+              Cro
+              <span>
+                <Image
+                  src="/svgs/logo-icon.svg"
+                  height={100}
+                  className="logo_img"
+                  width={100}
+                  alt="logo"
+                />
+              </span>{" "}
+              n <span className="red_text">Plan</span>
+            </div>
+          </Link>
 
           <div className="privacy_terms">
             <p>© 2020 Privacy — Terms</p>
@@ -44,10 +47,18 @@ const Footer = () => {
 
         <div className="footer_right_section">
           <ul className="flex_center">
-            <li>ABOUT</li>
-            <li>FAQS</li>
-            <li>TERMS & CONDITIONS</li>
-            <li>CONTACT</li>
+            <Link href="#about">
+              <li>About Us</li>
+            </Link>{" "}
+            <Link href="#wedding_plan">
+              <li>Services</li>
+            </Link>
+            <Link href="#real_wedding">
+              <li>Our Works</li>
+            </Link>
+            <Link href="#reviews">
+              <li>Reviews</li>
+            </Link>
           </ul>
         </div>
       </div>
